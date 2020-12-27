7a59b63733a0aa63f516e44db35edb50eb0c75e3
Max Zakharzhevskiy <maxzz@msn.com>
2020-12-27 02:59:15 -0800

    fix: GitHub deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import Icons, { ViteIconsResolver } from "vite-plugin-icons";
 import Components from "vite-plugin-components";
 
 const config: UserConfig = {
+    base: '/svg-icons-vite/',
     assetsDir: "static",
     plugins: [
         Components({
