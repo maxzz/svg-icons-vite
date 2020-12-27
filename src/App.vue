4a75e964620b996c2c160afedd8d6e0538f71f12
Max Zakharzhevskiy <maxzz@msn.com>
2020-12-26 17:04:56 -0800

    fix: clean up default project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import { defineComponent } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default defineComponent({
+    })
 </script>
