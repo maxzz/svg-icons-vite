91afbc6357a2d4a1b97aeb0629d997351b27d129
Max Zakharzhevskiy <maxzz@msn.com>
2020-12-26 17:16:01 -0800

    bug: fail to build

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
+    <div class="markdown-body" style="padding: 2em 3em">
+        <i-logos-vue style="font-size: 2em" />
+        <br />
+        <p>
+            Icons
+            <i-carbon-app-connectivity />
+            <i-mdi-account />
+            <mdi-alarm-off />
+            <i-fa-solid-dice-five />
+            from <code>vite-plugin-icons</code>
+        </p>
+    </div>
 </template>
 
-<script>
-    import { defineComponent } from "vue";
-
-    export default defineComponent({
-    })
+<script setup>
+    import MdiAlarmOff from "/@vite-icons/mdi/alarm-off.vue";
 </script>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { UserConfig } from "vite";
+import Icons, { ViteIconsResolver } from "vite-plugin-icons";
+import Components from "vite-plugin-components";
+
+const config: UserConfig = {
+    plugins: [
+        Components({
+            customComponentResolvers: [
+                ViteIconsResolver()
+            ],
+        }),
+        Icons(),
+    ],
+};
+
+export default config;
